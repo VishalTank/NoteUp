f1fb93a22b1972cdfe133a9f793371d28d3a03e2
Vishal Tank <tankvishal19101997@gmail.com>
2018-02-09 01:07:18 +0530

    Backend almost complete

---
+++ app/src/main/java/com/tank/vishal/noteup/AlarmReceiver.java
@@ -0,0 +1,62 @@
+package com.tank.vishal.noteup;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+        //Toast.makeText(context, "Alert : " + random_gen, Toast.LENGTH_LONG).show();
+
+        //notification builder
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.app_icon)
+                .setContentTitle("A Note needs to be reviewed!")
+                .setContentText(intent.getStringExtra("title"));
+
+
+        //View Note Action button in the notification.
+        Intent viewIntent = new Intent(context,MainActivity.class);
+
+        viewIntent.putExtra("setTitle",intent.getStringExtra("title"));
+        viewIntent.putExtra("setName",intent.getStringExtra("name"));
+        viewIntent.putExtra("setTime",intent.getStringExtra("time"));
+        viewIntent.putExtra("setReminderID",intent.getIntExtra("reminder_id",0));
+        viewIntent.putExtra("isSet","yes");
+
+        //viewIntent.setAction("View Note");
+        PendingIntent viewPendingIntent = PendingIntent.getActivity(context,intent.getIntExtra("reminder_id",0),viewIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        //mBuilder.addAction(0,"View Note",viewPendingIntent);
+        mBuilder.setContentIntent(viewPendingIntent);
+
+
+        //Add Note action button in the notification.
+        Intent addIntent = new Intent(context,EditNote.class);
+        //addIntent.setAction("Add Note");
+
+        PendingIntent addPendingIntent = PendingIntent.getActivity(context, intent.getIntExtra("reminder_id",0), addIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.addAction(0, "Add Note", addPendingIntent);
+
+
+        //Delete Note action button in the notification.
+
+
+        //Throws notifications.
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(intent.getIntExtra("reminder_id",0), mBuilder.build());
+    }
+}
